6e77ada55b6b84e4d658c328412a9692ebff5df6
Vishnu Dodballapur <vido2373@colorado.edu>
2021-11-11 21:21:47 -0700

    small write test

--- display_image/display_image.c
+++ display_image/display_image.c
@@ -7,11 +7,12 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdint.h>
 
 int main(void) {
     int lcd_spi_fd = 0;
     int sys_status = 0;
-
+    uint8_t test_array[2] = { 0x83, 0x87 };
     printf("Entering program...\n");
 
     lcd_spi_fd = open("/dev/spidev0.0", O_RDWR);
@@ -20,7 +21,19 @@ int main(void) {
         return 1;
     }
 
-    printf("Opened spi0.0\n");
+    printf("Opened spidev0.0\n");
+
+    sys_status = write(lcd_spi_fd, test_array, 2);
+    if (sys_status == -1) {
+        perror("write");
+        return 1;
+    }
+    else if (sys_status != 2) {
+        printf("Did not write full test_array\n");
+    }
+
+    printf("Check Logic Analyzer Output\n");
+
 
     sys_status = close(lcd_spi_fd);
     if (sys_status == -1) {
@@ -28,7 +41,7 @@ int main(void) {
         return 1;
     }
 
-    printf("Closed spi0.0\n");
+    printf("Closed spidev0.0\n");
 
     return 0;
 }
