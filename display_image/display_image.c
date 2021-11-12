afc86767d8e900ed6e91f9e96aedb32ce4b6114d
Vishnu Dodballapur <vido2373@colorado.edu>
2021-11-11 18:43:44 -0700

    basic spi test compiling

---
+++ display_image/display_image.c
@@ -0,0 +1,34 @@
+/*
+ * File name: display_image.c - Receives image from socket and displays on LCD
+ * Author: Vishnu Dodballapur
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(void) {
+    int lcd_spi_fd = 0;
+    int sys_status = 0;
+
+    printf("Entering program...\n");
+
+    lcd_spi_fd = open("/dev/spi0.0", O_RDWR);
+    if (lcd_spi_fd == -1) {
+        perror("open");
+        return 1;
+    }
+
+    printf("Opened spi0.0\n");
+
+    sys_status = close(lcd_spi_fd);
+    if (sys_status == -1) {
+        perror("close");
+        return 1;
+    }
+
+    printf("Closed spi0.0\n");
+
+    return 0;
+}
