76a5a0787ee461b7934497c2909740bf035ff1ae
Vishnu Dodballapur <vido2373@colorado.edu>
2021-11-12 16:19:01 -0700

    added ioctl commands to spi

--- display_image/display_image.c
+++ display_image/display_image.c
@@ -8,20 +8,50 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
 
 int main(void) {
     int lcd_spi_fd = 0;
+    int mode = SPI_MODE_0;
+    int speed = 1000000;
+    int bits = 8;
     int sys_status = 0;
     uint8_t test_array[2] = { 0x83, 0x87 };
     printf("Entering program...\n");
 
     lcd_spi_fd = open("/dev/spidev0.0", O_RDWR);
-    if (lcd_spi_fd == -1) {
+    if (lcd_spi_fd < 0) {
         perror("open");
         return 1;
     }
 
     printf("Opened spidev0.0\n");
+
+    sys_status = ioctl(lcd_spi_fd, SPI_IOC_WR_MODE, &mode);
+    if (sys_status < 0) {
+        perror("SPI_IOC_WR_MODE");
+        return 1;
+    }
+
+    printf("Set WR mode\n");
+
+    sys_status = ioctl(lcd_spi_fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
+    if (sys_status < 0) {
+        perror("SPI_IOC_WR_BITS_PER_WORD");
+        return 1;
+    }
+
+    printf("Set WR bits per word\n");
+
+    sys_status = ioctl(lcd_spi_fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+    if (sys_status < 0) {
+        perror("SPI_IOC_WR_MAX_SPEED_HZ");
+        return 1;
+    }
+
+    printf("Set WR speed\n");
+
 
     sys_status = write(lcd_spi_fd, test_array, 2);
     if (sys_status == -1) {
