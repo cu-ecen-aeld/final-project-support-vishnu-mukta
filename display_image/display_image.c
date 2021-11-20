d17d6bb0951d320ad99f85051d64208a4417e900
Vishnu Dodballapur <vido2373@colorado.edu>
2021-11-19 21:23:22 -0700

    client code sending and receiving successfully from socket server

--- display_image/display_image.c
+++ display_image/display_image.c
@@ -25,6 +25,7 @@ int main(int argc, char* argv[]) {
     int socket_fd;
     struct addrinfo hints;
     struct addrinfo* res;
+    uint8_t recv_buff[15] = { 0 };
 
     if (argc != 2) {
         printf("Incorrect number of arguments.\nUsage:\n./display_image [ server ip address ] \n");
@@ -57,6 +58,28 @@ int main(int argc, char* argv[]) {
     }
 
     printf("Connected!\n");
+
+    int send_bytes;
+    send_bytes = send(socket_fd, "Hello, Mukta!\n", 14, 0);
+    if (send_bytes < 0) {
+        syslog(LOG_ERR, "send");
+        perror("send");
+        exit(-1);
+    }
+
+    printf("Bytes sent: %d\n", send_bytes);
+
+    int recv_bytes;
+    recv_bytes = recv(socket_fd, recv_buff, 14, 0);
+    if (recv_bytes < 0) {
+        syslog(LOG_ERR, "recv");
+        perror("recv");
+        exit(-1);
+    }
+
+    printf("Bytes recvd: %d\n", recv_bytes);
+    printf("Message received from socket server: %s\n", (char *)recv_buff);
+
 
     /*
     uint16_t color = 0xFFE0;
