f5878ba3bc4b1e8b362aa9c29e52c4a2eb66e8a8
MuktaDarekar <mukta.darekar@colorado.edu>
2021-11-19 21:00:07 -0700

    updated aesdsocket.c to remove all assignment8 modifications

--- socket_server/aesdsocket.c
+++ socket_server/aesdsocket.c
@@ -30,95 +30,39 @@
 #define ERROR		1
 #define SUCCESS		0
 #define MYPORT		9000
-#define BUFFER_LEN	200
-
-#define USE_AESD_CHAR_DEVICE 1
-
-#ifdef USE_AESD_CHAR_DEVICE
-#define DEF_FILEPATH	"/dev/aesdchar"
-#else
 #define DEF_FILEPATH	"/var/tmp/aesdsocketdata"
-#endif
+#define BUFFER_LEN	200
 
 typedef struct
 {
-	pthread_t thread;
-	int fd;
-	int acceptedfd;
+    pthread_t thread;
+    int fd;
+    int acceptedfd;
 	struct in_addr sin_addr;
-	bool thread_complete_success;
-	bool complete_status_flag;
+    bool thread_complete_success;
+    bool complete_status_flag;
+
 }thread_data;
 
 
 typedef struct slist_data_s slist_data_t;
-struct slist_data_s
-{
-	thread_data params;
-	SLIST_ENTRY(slist_data_s) entries;
+struct slist_data_s{
+
+    thread_data params;
+    SLIST_ENTRY(slist_data_s) entries;
 };
 
 int sockfd;
 int exit_on_signal=0;
 
-pthread_mutex_t rwmutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-
-typedef struct 
-{
-	int fd;
-	bool timer_thread_success;
-}timer_thread_data;
-
-#ifndef USE_AESD_CHAR_DEVICE
-// A thread which runs every 10sec of interval
-// Assumes timer_create has configured for sigval.sival_ptr to point to the
-// thread data used for the timer
-static void timer_thread(union sigval sigval)
-{
-
-	timer_thread_data* td = (timer_thread_data*) sigval.sival_ptr;
-
-	td->timer_thread_success = true;
-
-	char tmrbuffer[BUFFER_LEN];
-	time_t rtime;
-	time(&rtime);
-	struct tm *timestamp = localtime(&rtime);
-	size_t nbytes;
-
-	if((nbytes = strftime(tmrbuffer, BUFFER_LEN, "timestamp:%a, %d %b %Y %T %z\n", timestamp)) == 0)
-	{
-		syslog(LOG_ERR,"timestamp to string failed");
-		td->timer_thread_success = false;
-	}
-
-	if(pthread_mutex_lock(&rwmutex) != 0)
-	{
-		syslog(LOG_ERR, "pthread_mutex_lock failed");
-		td->timer_thread_success = false;
-	}
-
-	if(write(td->fd, tmrbuffer, nbytes) == -1)
-	{
-		syslog(LOG_ERR,"timestamp write failed");
-		td->timer_thread_success = false;
-	}
-
-	if(pthread_mutex_unlock(&rwmutex) != 0)
-	{
-		syslog(LOG_ERR, "pthread_mutex_unlock failed");
-		td->timer_thread_success = false;
-	}
-
-//pthread_exit(NULL);
-}
-#endif
 
 //Function:	static void signal_handler(int signo)
 //Inputs:	signo - Signal number
 static void signal_handler(int signo)
 {
+
 	syslog(LOG_DEBUG, "in handler\n");
 	if(signo == SIGINT || signo==SIGTERM) 
 	{
@@ -126,167 +70,159 @@ static void signal_handler(int signo)
 			syslog(LOG_DEBUG, "Caught signal SIGINT, exiting\n");
 		else
 			syslog(LOG_DEBUG, "Caught signal SIGTERM, exiting\n");
-
+			
 		shutdown(sockfd, SHUT_RDWR);
 
 		exit_on_signal = true;
 	}
+	
 }
 
-void* packetRWthread(void* thread_param)
+void packetRWthread(void* thread_param)
 {
-	thread_data *thread_func_args = (thread_data*)thread_param;
+    thread_data *thread_func_args = (thread_data*)thread_param;
 	bool status = true;
 
-	int req_size=0;
-	int size = BUFFER_LEN;    
-	char *rebuffer = NULL;
-	char *buffer = (char*)malloc(sizeof(char)*BUFFER_LEN);
+    int req_size=0;
+    int size = BUFFER_LEN;    
+    char *rebuffer = NULL;
+    char *buffer = (char*)malloc(sizeof(char)*BUFFER_LEN);
 	int nbytes = 0;
 	ssize_t nr = 0;
-	//int fd = 0;
 
 	sigset_t mask;
-	//Create signal set
-	if (sigemptyset(&mask) == -1) 
+    //Create signal set
+    if (sigemptyset(&mask) == -1) 
 	{
-		syslog(LOG_ERR, "creating empty signal set failed");
+        syslog(LOG_ERR, "creating empty signal set failed");
 		status = false;
-	}
+    }
 	//Add signal SIGINT into created empty set
-	if (sigaddset(&mask, SIGINT) == -1) 
+    if (sigaddset(&mask, SIGINT) == -1) 
 	{
-		syslog(LOG_ERR, "Adding SIGINT failed");
+        syslog(LOG_ERR, "Adding SIGINT failed");
 		status = false;
-	}
+    }
 	//Add signal SIGTERM into created empty set
-	if (sigaddset(&mask, SIGTERM) == -1) 
+    if (sigaddset(&mask, SIGTERM) == -1) 
 	{
-		syslog(LOG_ERR, "Adding SIGTERM failed");
+        syslog(LOG_ERR, "Adding SIGTERM failed");
 		status = false;
-	} 
-	
-	//create or open file to store received packets
-	thread_func_args->fd = open(DEF_FILEPATH, O_CREAT | O_RDWR | O_TRUNC, 0666);
-	if (thread_func_args->fd == -1)	
-	{//if error
-		syslog(LOG_ERR, "can't open or create file '%s'\n", DEF_FILEPATH);
-		status = false;
-	}
-
-	while(1)
-	{
-		nbytes = recv(thread_func_args->acceptedfd, buffer+req_size, BUFFER_LEN, 0);
-		if(nbytes == -1)
-		{
-			syslog(LOG_ERR,"receive failed");
+    } 
+       
+    
+    while(1)
+    {
+    	nbytes = recv(thread_func_args->acceptedfd, buffer+req_size, BUFFER_LEN, 0);
+    	if(nbytes == -1)
+    	{
+		    syslog(LOG_ERR,"receive failed");
 			status = false;
 			break;
-		}
-		if (nbytes ==0)
-			break;
-
-		req_size = req_size + nbytes;
-
-		if(size < (req_size+1))
-		{
-			size += BUFFER_LEN;
-			rebuffer = realloc(buffer,sizeof(char)*size);
-			if(rebuffer == NULL)
-			{
-				syslog(LOG_ERR,"realloc failed");
+        }
+        if (nbytes ==0)
+        	break;
+        
+        req_size = req_size + nbytes;
+        	
+        if(size < (req_size+1))
+        {
+        	size += BUFFER_LEN;
+        	rebuffer = realloc(buffer,sizeof(char)*size);
+        	if(rebuffer == NULL)
+        	{
+		        syslog(LOG_ERR,"realloc failed");
 				status = false;
 				break;
-			}
-			buffer = rebuffer;
-		}
-
-		if(strchr(buffer,'\n') != NULL) 
-		break;    	
-	}
-
-	if(pthread_mutex_lock(&rwmutex) != 0)
+		    }
+		    buffer = rebuffer;
+        }
+       	
+       	if(strchr(buffer,'\n') != NULL) 
+       		break;    	
+    }
+    
+    if(pthread_mutex_lock(&mutex) != 0)
 	{
 		syslog(LOG_ERR, "pthread_mutex_lock failed");
 		status = false;
 	}
-	// Block Signals
-	if (sigprocmask(SIG_BLOCK,&mask,NULL) == -1)
-	{
-		syslog(LOG_ERR,"sigprocmask failed");
+    // Block signals to avoid partial write
+    if (sigprocmask(SIG_BLOCK,&mask,NULL) == -1)
+    {
+        syslog(LOG_ERR,"sigprocmask failed");
 		status = false;
-	}	
+    }	
 	nr = write(thread_func_args->fd, buffer, req_size);
 	if (nr == -1)	
 	{//if error
 		syslog(LOG_ERR, "can't write received string in file '%s'", DEF_FILEPATH);
 		status = false;
 	}
-	// Unblock signals
-	if (sigprocmask(SIG_UNBLOCK,&mask,NULL) == -1)
-	{
-		syslog(LOG_ERR,"sigprocmask failed");
+    // Block signals to avoid partial write
+    if (sigprocmask(SIG_UNBLOCK,&mask,NULL) == -1)
+    {
+        syslog(LOG_ERR,"sigprocmask failed");
 		status = false;
-	}		
-	if(pthread_mutex_unlock(&rwmutex) != 0)
+    }		
+	if(pthread_mutex_unlock(&mutex) != 0)
 	{
 		syslog(LOG_ERR, "pthread_mutex_unlock failed");
 		status = false;
 	}
+    
 
-	#ifndef USE_AESD_CHAR_DEVICE
-	lseek(thread_func_args->fd, 0, SEEK_SET);
-	#endif
-
-	req_size = 0;
+    lseek(thread_func_args->fd, 0, SEEK_SET);
+    
+    req_size = 0;
 	int ptr=0;
-
-	while(1)
-	{
-
-		if(pthread_mutex_lock(&rwmutex) != 0)
+    
+    while(1)
+    {
+     
+		if(pthread_mutex_lock(&mutex) != 0)
 		{
 			syslog(LOG_ERR, "pthread_mutex_lock failed");
 			status = false;
 		}
 		if (sigprocmask(SIG_BLOCK,&mask,NULL) == -1)
 		{
-			syslog(LOG_ERR,"sigprocmask failed");
+		    syslog(LOG_ERR,"sigprocmask failed");
 			status = false;
 		}
-
-		nr = read(thread_func_args->fd, &buffer[ptr], 1);    
-
+    
+    	nr = read(thread_func_args->fd, &buffer[ptr], 1);    
+    
 		if (sigprocmask(SIG_UNBLOCK,&mask,NULL) == -1)
 		{
-			syslog(LOG_ERR,"sigprocmask failed");
+		    syslog(LOG_ERR,"sigprocmask failed");
 			status = false;
 		}
-
-		if(pthread_mutex_unlock(&rwmutex) != 0)
+		
+		if(pthread_mutex_unlock(&mutex) != 0)
 		{
 			syslog(LOG_ERR, "pthread_mutex_unlock failed");
 			status = false;
 		}    
-
-		if (nr == 1)
-		{        	  
-			if(buffer[ptr] == '\n')
-			{
-				req_size = (ptr+1);
-				nbytes = send(thread_func_args->acceptedfd, buffer, req_size, 0);
-				if(nbytes != req_size)
-				{
+		
+        if (nr == 1)
+        {        	  
+        	if(buffer[ptr] == '\n')
+        	{
+        		req_size = (ptr+1);
+        		nbytes = send(thread_func_args->acceptedfd, buffer, req_size, 0);
+        		if(nbytes != req_size)
+        		{
 					syslog(LOG_ERR, "send failed");
 					break;
-				}
-				ptr=0;
-				memset(buffer, 0, req_size);
-			}
-			else
-			{
-				ptr++;
-
+        		}
+        		ptr=0;
+        		memset(buffer, 0, req_size);
+        	}
+        	else
+        	{
+        		ptr++;
+        	
 				if(size < (ptr+1))
 				{
 					size += BUFFER_LEN;
@@ -299,78 +235,79 @@ void* packetRWthread(void* thread_param)
 					}
 					buffer = rebuffer;
 				}
-			}
-		}
-		else if (nr == 0)
-		{
-			syslog(LOG_DEBUG, "read done");
-			break;
-		}
-		else
-		{
-			syslog(LOG_ERR, "read failed");
+        	}
+        	
+        }
+        else if (nr == 0)
+        {
+        	syslog(LOG_DEBUG, "read done");
+        	break;
+        }
+        else
+        {
+        	syslog(LOG_ERR, "read failed");
 			status = false;
 			break;
-		}
-	}
-
-	if (status == true)
-	{
+        }
+    	
+    }
+    
+    if (status == true)
+    {
 		syslog(LOG_DEBUG, "Successful");
-	}
-
+    }
+    
 	syslog(LOG_DEBUG, "Closing connection from '%s'\n", inet_ntoa((struct in_addr)thread_func_args->sin_addr));
-	close(thread_func_args->acceptedfd);
-	close(thread_func_args->fd);
+    close(thread_func_args->acceptedfd);
 
-	//status true
+    //status true
 	thread_func_args->thread_complete_success = status;
-	thread_func_args->complete_status_flag = true;
+    thread_func_args->complete_status_flag = true;
 
-	free(buffer);
-	free(rebuffer);
-
-	//pthread_exit(NULL);
-	return NULL;
+    free(buffer);
+    free(rebuffer);
+    
+    pthread_exit(NULL);
 }
 
 
 int main(int argc, char* argv[])
 {
-	openlog(NULL, LOG_CONS, LOG_USER);
+
+    openlog(NULL, LOG_CONS, LOG_USER);
 
 	struct sockaddr_in saddr;
 	bool exit_on_error = false;
 	int opt=1;
 	int acceptedfd;
-	//int fd; 
-	socklen_t len;
-	int ret = 0;
+	int fd; 
+    socklen_t len;
+    int ret = 0;
 
 	slist_data_t *datap = NULL;
 	SLIST_HEAD(slisthead,slist_data_s) head;
-	SLIST_INIT(&head);
-
+    SLIST_INIT(&head);
+            
 	syslog(LOG_INFO, "aesdsocket code started\n");
-
+	
 	// check if deamon needs to be started
-	if ((argc == 2) && (strcmp("-d", argv[1])==0)) 
+    if ((argc == 2) && (strcmp("-d", argv[1])==0)) 
 	{
 		// start deamon
-		pid_t pid = fork();
-		if (pid == -1) 
+        pid_t pid = fork();
+        if (pid == -1) 
 		{
-			syslog(LOG_ERR, "failed to fork");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid > 0)
+            syslog(LOG_ERR, "failed to fork");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid > 0)
 		{
-			exit(EXIT_SUCCESS);
-		}
+            exit(EXIT_SUCCESS);
+        }
 
 		syslog(LOG_INFO, "fork successful\n");
-
-		pid_t sid = setsid();		
+	
+        pid_t sid = setsid();		
 		if (sid == -1) 
 		{
 			syslog(LOG_ERR, "failed to setsid");
@@ -389,10 +326,10 @@ int main(int argc, char* argv[])
 		close(STDIN_FILENO);
 		close(STDOUT_FILENO);
 		close(STDERR_FILENO);
-
+		
 		syslog(LOG_INFO, "daemon created\n");
-	}
-
+    }
+    
 	// Set signal handler for SIGINT
 	if(signal(SIGINT, signal_handler) == SIG_ERR)
 	{
@@ -408,7 +345,7 @@ int main(int argc, char* argv[])
 		exit_on_error = true;
 		goto EXITING;
 	}
-
+    
 	//create socket
 	sockfd = socket(PF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1)
@@ -418,7 +355,7 @@ int main(int argc, char* argv[])
 		goto EXITING;
 	}
 	syslog(LOG_INFO, "socket created\n");
-
+	
 	//reuse socket
 	if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(int)) == -1)
 	{
@@ -440,8 +377,8 @@ int main(int argc, char* argv[])
 		goto EXITING;
 	}
 	syslog(LOG_INFO, "bind successful\n");
-
-	//start listening at port 9000
+    
+    //start listening at port 9000
 	ret = listen(sockfd, 10);
 	if (ret == -1)
 	{
@@ -451,80 +388,26 @@ int main(int argc, char* argv[])
 	}
 	syslog(LOG_INFO, "listening\n");
 
-/*
 	//create or open file to store received packets
-	fd = open(DEF_FILEPATH, O_CREAT | O_RDWR | O_APPEND | O_TRUNC, 0666);
+	fd = open(DEF_FILEPATH, O_CREAT | O_RDWR | O_APPEND | O_TRUNC, 0764);
 	if (fd == -1)	
 	{//if error
 		syslog(LOG_ERR, "can't open or create file '%s'\n", DEF_FILEPATH);
 		exit_on_error = true;
 		goto EXITING;
 	}
+    
+    while(exit_on_signal==0) 
+    {
 
-*/
-#ifndef USE_AESD_CHAR_DEVICE
-	timer_t timerid;
-	struct sigevent *sev = malloc(sizeof(struct sigevent));
-	timer_thread_data td;
-	int clock_id = CLOCK_MONOTONIC;
-
-	memset(sev,0,sizeof(struct sigevent));
-	memset(&td,0,sizeof(timer_thread_data));
-	td.fd = fd;
-	td.timer_thread_success = true;
-
-	//Setup a call to timer_thread passing in the td structure as the sigev_value argument
-	sev->sigev_notify = SIGEV_THREAD;
-	sev->sigev_value.sival_ptr = &td;
-	sev->sigev_notify_function = timer_thread;
-
-	if (timer_create(clock_id, sev, &timerid) != 0 ) 
-	{
-		syslog(LOG_ERR, "can't create timer_thread");
-		exit_on_error = true;
-		goto EXITING;
-	}
-	else
-	{
-		struct timespec timer_val;
-		ret = clock_gettime(clock_id, &timer_val);
-		if(ret == -1)
-		{
-			syslog(LOG_ERR, "clock_gettime failed");
-			exit_on_error = true;
-			goto EXITING;
-		}
-		struct itimerspec interval;
-		interval.it_interval.tv_sec = 10;
-		interval.it_interval.tv_nsec = 0;
-
-		interval.it_value.tv_sec = timer_val.tv_sec + interval.it_interval.tv_sec;
-		interval.it_value.tv_nsec = timer_val.tv_nsec + interval.it_interval.tv_nsec;
-		if( interval.it_value.tv_nsec > 1000000000L ) 
-		{
-			interval.it_value.tv_nsec -= 1000000000L;
-			interval.it_value.tv_sec += 1;
-		}
-
-		if(timer_settime(timerid, TIMER_ABSTIME, &interval, NULL ) != 0 ) 
-		{		   
-			syslog(LOG_ERR, "timer_settime failed");
-			exit_on_error = true;
-			goto EXITING;
-		} 
-	}
-#endif	
-
-	while(exit_on_signal==0) 
-	{
 		len = sizeof(struct sockaddr);
-
+		
 		//accept connection
 		acceptedfd = accept(sockfd, (struct sockaddr *) &saddr, &len);
-
+		
 		if(exit_on_signal || exit_on_error)
 			break;
-
+			
 		if (acceptedfd == -1)
 		{
 			syslog(LOG_ERR, "socket accepting failed\n");
@@ -533,7 +416,7 @@ int main(int argc, char* argv[])
 		}
 		if(exit_on_signal || exit_on_error)
 			break;
-
+			
 		syslog(LOG_DEBUG, "Accepted connection from '%s'\n", inet_ntoa((struct in_addr)saddr.sin_addr));
 
 		datap = malloc(sizeof(slist_data_t));
@@ -543,26 +426,26 @@ int main(int argc, char* argv[])
 			exit_on_error = true;
 			goto EXITING;
 		}
-
+			
 		SLIST_INSERT_HEAD(&head,datap,entries);
 		datap->params.acceptedfd = acceptedfd;
 		datap->params.complete_status_flag = false;
 		datap->params.thread_complete_success = false;
 		datap->params.sin_addr = saddr.sin_addr;
-		//datap->params.fd=fd;
+		datap->params.fd=fd;
 
-		pthread_create(&(datap->params.thread), NULL, &packetRWthread, (void*)&(datap->params));
+		pthread_create(&(datap->params.thread), NULL, (void*)&packetRWthread, (void*)&(datap->params));
 
 		SLIST_FOREACH(datap,&head,entries)
 		{
-			if (datap->params.complete_status_flag == true)
-				pthread_join(datap->params.thread,NULL);
-			else
-				continue;
-		}
+		    if (datap->params.complete_status_flag == true)
+		        pthread_join(datap->params.thread,NULL);
+		    else
+		    	continue;
+    	}
 	}
 
-	EXITING:
+EXITING:
 
 	if (exit_on_error && exit_on_signal==false)
 		syslog(LOG_DEBUG, "exiting on failure\n");
@@ -575,51 +458,37 @@ int main(int argc, char* argv[])
 	if(sockfd)
 		close(sockfd);
 
-
-#ifndef USE_AESD_CHAR_DEVICE
 	if(fd)	
 	{
 		close(fd);
 		remove(DEF_FILEPATH);
 	}
-#endif
-
+	
 	SLIST_FOREACH(datap,&head,entries)
 	{
-		if (datap->params.complete_status_flag == false)
-			pthread_cancel(datap->params.thread);
+	    if (datap->params.complete_status_flag == false)
+	    	pthread_cancel(datap->params.thread);
 	}
 
 	while (!SLIST_EMPTY(&head)) 
 	{
-		datap = SLIST_FIRST(&head);
-		SLIST_REMOVE_HEAD(&head, entries);
-		free(datap);
+        datap = SLIST_FIRST(&head);
+        SLIST_REMOVE_HEAD(&head, entries);
+        free(datap);
 		datap = NULL;
-	}
+    }
 
-#ifndef USE_AESD_CHAR_DEVICE
-	if(td.timer_thread_success == true)
-	{
-		syslog(LOG_DEBUG,"deleting timerid");
-		if (timer_delete(timerid) != 0) 
-		{
-			syslog(LOG_ERR,"Error in deleting timerid");
-			exit_on_error = true;
-		}
-	}
-	
-	free(sev);
-#endif
-
-	pthread_mutex_destroy(&rwmutex);
+	pthread_mutex_destroy(&mutex);
 	closelog();
-
+	
 	if (exit_on_error && exit_on_signal==false)
 		return 1;
 	else
 		return 0;
+		
 }
+
+
 
 
 
